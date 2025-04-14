7ef0eb17e97b05902ccfe6f01a78dce99dc2bfb0
Nag <nagclouslab@gmail.com>
2025-04-14 17:23:11 +0530

    ex1

--- 1-play-with-java/src/main/java/com/example/exercise/Ex1.java
+++ 1-play-with-java/src/main/java/com/example/exercise/Ex1.java
@@ -1,4 +1,26 @@
 package com.example.exercise;
 
 public class Ex1 {
+    public static void main(String[] args) {
+
+        // input
+        String[] csvReport = {
+                "A,IT,1000",
+                "B,HR,2000",
+                "C,IT,3000",
+                "D,HR,4000",
+        };
+
+        // output
+
+        /*
+            Department | Total Salary
+            IT          | 4000
+            HR          | 6000
+
+         */
+
+
+
+    }
 }
